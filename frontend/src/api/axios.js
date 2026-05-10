5539f2c57bb22faf857485cc190b146af47458f5
Ayub <ayub@Ayubs-MacBook-Pro-3.local>
2026-05-10 21:27:55 +0800

    api axios update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://38.47.180.195/student07/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
